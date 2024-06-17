1a93b0403803b3015310355272aefe8b778ff373
alisherov-cs <alisherov.cs@gmail.com>
2024-06-17 23:34:34 +0500

    feat: piece color

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "board.cpp"
 #include "piece.cpp"
-using namespace std;
 
 int main() {
     Board board;
-    Piece piece('a', 1);
-    cout << "x: " << piece.getPosition().x << endl;
-    cout << "y: " << piece.getPosition().y << endl;
-    cout << board.chessBoard[3][4] << endl;
+    Piece piece('a', 1, "white");
+    std::cout << "x: " << piece.getPosition().x << std::endl;
+    std::cout << "y: " << piece.getPosition().y << std::endl;
+    std::cout << board.chessBoard[3][4] << std::endl;
+    std::cout << piece.getColor() << std::endl;
 }
--- piece.cpp
+++ piece.cpp
@@ -1,16 +1,22 @@
 #include "types.cpp"
+#include <string>
 
 class Piece {
     private:
-        char _x;
-        int _y;
+        char x;
+        int y;
+        std::string color;
     public:
         Position getPosition() {
-            return Position { _x, _y };
+            return Position { this->x, this->y };
         } 
+        std::string getColor() {
+            return this->color;
+        }
 
-        Piece(int x, char y) {
-            _x = x;
-            _y = y;
+        Piece(int x, char y, std::string color) {
+            this->x = x;
+            this->y = y;
+            this->color = color;
         }
 };
