892f5635213ebf3f97e8fc721d920ad08015d646
alisherov-cs <alisherov.cs@gmail.com>
2024-06-20 23:17:53 +0500

    feat: pawn

--- piece.cpp
+++ piece.cpp
@@ -7,16 +7,26 @@ class Piece {
         int y;
         std::string color;
     public:
-        Position getPosition() {
-            return Position { this->x, this->y };
-        } 
-        std::string getColor() {
-            return this->color;
-        }
-
         Piece(int x, char y, std::string color) {
             this->x = x;
             this->y = y;
             this->color = color;
         }
+
+        Position getPosition() {
+            return Position { this->x, this->y };
+        } 
+
+        std::string getColor() {
+            return this->color;
+        }
+
+        void move(char x, int y) {
+            // movement...
+        }
+};
+
+class Pawn : public Piece {
+    public:
+        Pawn(char x, int y, std::string color) : Piece(x, y, color) {}
 };
